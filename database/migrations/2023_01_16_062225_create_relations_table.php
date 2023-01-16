41ae4347beaf269bd50e223631b631d44368ebe9
Kai Sato <kaisato@Kais-MacBook-Pro.local>
2023-01-16 14:33:40 +0800

    add relations table

---
+++ database/migrations/2023_01_16_062225_create_relations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('relations', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id_1')->unsigned();
+            $table->foreign('user_id_1')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('user_id_2')->unsigned();
+            $table->foreign('user_id_2')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('relations');
+    }
+};
